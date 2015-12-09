8aeebe60b8a35265f3cdcd661c1666fbb3404bf6
Henry J Schmale <hschmale16@gmail.com>
2015-12-09 13:12:38 -0500

    Work on the thing some more

--- main.cpp
+++ main.cpp
@@ -4,14 +4,41 @@
 #include <fstream>
 #include <algorithm>
 #include "tree.hh"
+//#include "tree_util.hh"
 
 struct letter {
-    uint8_t l : 8; // the letter
+    uint32_t l : 8; // the letter
     uint32_t count : 24;
+
+    letter(){
+        l = 0;
+        count = 0;
+    }
+
+    letter(char c){
+        l = c;
+        count = 0;
+    }
 };
 
-void initMarkovTree(tree<letter>& tr, int depth){
+void initMarkovTreeNode(tree<letter>& tr,
+        tree<letter>::iterator& it, int depth){
+    for(char c = 'A'; c <= 'Z'; c++){
+        tr.append_child(it, letter(c));
+        if(depth > 0){
+            initMarkovTreeNode(tr, it, depth - 1);
+        }
+    }
+}
 
+void initMarkovTree(tree<letter>& tr, int depth){
+    for(char c = 'A'; c <= 'Z'; c++){
+        tree<letter>::iterator node = tr.insert(tr.begin(), letter(c));
+        if(depth > 0){
+            initMarkovTreeNode(tr, node, depth - 1);
+            std::cout << c << std::endl;
+        }
+    }
 }
 
 int main(int argc, char** argv){
@@ -22,8 +49,8 @@ int main(int argc, char** argv){
     }
     // build markov chain for words
     tree<letter> tr;
-    initMarkovTree(tr, 20);
     std::string word;
+    initMarkovTree(tr, 10);
     while(infile >> word){
     }
 }
