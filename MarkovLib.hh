bbc2638700f80d57f7f992a9571c7c36e1b835b1
Henry J Schmale <hschmale16@gmail.com>
2015-12-14 13:54:40 -0500

    Added new data set and split off into header
    * Split the library into a header
    * Added a sentences dataset.
    * Worked on making a markov chain based sentence generator

---
+++ MarkovLib.hh
@@ -0,0 +1,124 @@
+#ifndef MARKOV_LIB_H_INC
+#define MARKOVTREE_H_INC
+
+#include <vector>
+#include <random>
+#include <map>
+#include <iterator>
+#include <chrono>
+#include <iostream>
+#include <string>
+
+static std::default_random_engine gen =
+    std::default_random_engine(std::chrono::system_clock::now()
+        .time_since_epoch().count()
+    );
+
+size_t randomZeroToN(size_t n){
+    std::uniform_int_distribution<size_t> d(0, n);
+    return d(gen);
+}
+
+short twoChar2Short(char c1, char c2){
+    short s = (c1 << 8) | (c2);
+    return s;
+}
+
+class LetterCombo {
+public:
+    char first;
+    char second;
+    //int  count;
+
+    LetterCombo(char c1, char c2)
+        :first(c1), second(c2){}
+    ~LetterCombo() {}
+
+    bool operator<(const LetterCombo& rhs) const{
+        short s1 = twoChar2Short(first, second);
+        short s2 = twoChar2Short(rhs.first, rhs.second);
+        return (s1 < s2);
+    }
+
+    bool operator==(const LetterCombo& r){
+        return (first == r.first) && (second == r.second);
+    }
+};
+
+class WordMarkovChain {
+private:
+    int _trainingSize;
+    int _wordLengthAcc;
+    std::map<LetterCombo, uint64_t> _map;
+
+    LetterCombo selectNextCombo(char c){
+        std::vector<LetterCombo> combos;
+        std::vector<uint64_t> probs;
+        auto mapIt = _map.begin();
+        while(mapIt != _map.end()){
+            if(mapIt->first.first == c){
+                combos.push_back(mapIt->first);
+                probs.push_back(mapIt->second);
+                //std::cerr << "match" << std::endl;
+            }
+            ++mapIt;
+        }
+        if(combos.size() <= 1){
+            return LetterCombo(0,0); // insufficent choices tell it to end gen
+        }
+        std::discrete_distribution<uint64_t> distrib(probs.begin(),probs.end());
+        uint64_t idx = distrib(gen);
+        return combos[idx];
+    }
+public:
+    WordMarkovChain() {
+        _trainingSize = 0;
+        _wordLengthAcc = 0;
+    }
+
+    ~WordMarkovChain(){}
+
+    void train(const std::string& w){
+        _trainingSize++;
+        // iterate and build list
+        int sz = w.length();
+        for(int i = 1; i < sz; i++) {
+            _map[LetterCombo(w[i - 1], w[i])]++;
+        }
+        _wordLengthAcc += sz;
+        //std::cerr << _map.size() << std::endl;
+    }
+
+    LetterCombo getRandomCombo(){
+        std::map<LetterCombo, uint64_t>::iterator it = _map.begin();
+        std::advance(it, randomZeroToN(_map.size()));
+        return it->first;
+    }
+
+    std::string generate(){
+        int sz = _wordLengthAcc / _trainingSize;
+        std::string str;
+        LetterCombo c = this->getRandomCombo();
+        str += c.first;
+        for(int i = 1; i < sz; i++){
+            c = this->selectNextCombo(c.second);
+            if(c == LetterCombo(0, 0)){
+                return str;
+            }
+            str += c.first;
+        }
+        return str;
+    }
+
+    friend std::ostream& operator<<(std::ostream& out,
+            const WordMarkovChain& c){
+        out << "Trained on: " << c._trainingSize << " words" << std::endl;
+        out << "Characters: " << c._wordLengthAcc << std::endl;
+        out << "AvgWordLen: " << c._wordLengthAcc / c._trainingSize
+            << std::endl;
+        out << "Prob Keys : " << c._map.size() << std::endl;
+        return out;
+    }
+};
+
+#endif //MARKOVTREE_H_INC
